b471553c9ae968e3b0c6da755194558d3cd5c1c7
Jay Piamjariyakul <j.piamjariyakul@outlook.com>
2019-11-17 18:50:42 +0000

    Added repo files

---
+++ part1_group08/Debug/inc/motor.d
@@ -0,0 +1,37 @@
+# FIXED
+
+inc/motor.obj: ../inc/motor.c
+inc/motor.obj: C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h
+inc/motor.obj: C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h
+inc/motor.obj: C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h
+inc/motor.obj: C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h
+inc/motor.obj: C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h
+inc/motor.obj: C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h
+inc/motor.obj: C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h
+inc/motor.obj: C:/TI/ccs/ccsv8/ccs_base/arm/include/msp.h
+inc/motor.obj: C:/TI/ccs/ccsv8/ccs_base/arm/include/msp432p401r.h
+inc/motor.obj: C:/TI/ccs/ccsv8/ccs_base/arm/include/msp_compatibility.h
+inc/motor.obj: C:/TI/ccs/ccsv8/ccs_base/arm/include/msp432p401r_classic.h
+inc/motor.obj: C:/TI/ccs/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h
+inc/motor.obj: C:/TI/ccs/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+inc/motor.obj: C:/TI/ccs/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+inc/motor.obj: C:/TI/ccs/ccsv8/ccs_base/arm/include/system_msp432p401r.h
+inc/motor.obj: ../inc/SysTick.h
+
+../inc/motor.c: 
+C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h: 
+C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h: 
+C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h: 
+C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h: 
+C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h: 
+C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h: 
+C:/TI/ccs/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h: 
+C:/TI/ccs/ccsv8/ccs_base/arm/include/msp.h: 
+C:/TI/ccs/ccsv8/ccs_base/arm/include/msp432p401r.h: 
+C:/TI/ccs/ccsv8/ccs_base/arm/include/msp_compatibility.h: 
+C:/TI/ccs/ccsv8/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/TI/ccs/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/TI/ccs/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/TI/ccs/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/TI/ccs/ccsv8/ccs_base/arm/include/system_msp432p401r.h: 
+../inc/SysTick.h: 
